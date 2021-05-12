7e7aaafdfb8f3e5d0b4dcddb49802291d089cd2c
Kevin Skayro <kev1n-1997@outlook.com>
2021-05-12 08:59:48 -0400

    fixed bug that caused the popups to close when clicking inside content window

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //open project's popups start
 const openPopupBtn = document.querySelectorAll("[data-popup-target]");
 const closePopupBtn = document.querySelectorAll("[data-close-popup]");
+const popupOverlay = document.querySelector("[data-popup-overlay]");
 
 openPopupBtn.forEach((button) => {
   button.addEventListener("click", () => {
@@ -15,14 +16,23 @@ closePopupBtn.forEach((button) => {
   });
 });
 
+popupOverlay.addEventListener("click", () => {
+  const popups = document.querySelectorAll(".popup.active");
+  popups.forEach((popup) => {
+    closePopUp(popup);
+  });
+});
+
 function openPopUp(popup) {
   if (popup == null) return;
   popup.classList.add("active");
+  popupOverlay.classList.add("active");
 }
 
 function closePopUp(popup) {
   if (popup == null) return;
   popup.classList.remove("active");
+  popupOverlay.classList.remove("active");
 }
 //open project's popups end
 
